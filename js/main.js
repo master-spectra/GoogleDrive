a247eb4d13fd0fd69cc74813f880f8ea98bd69aa
master-spectra <63547718+master-spectra@users.noreply.github.com>
2020-08-21 12:20:58 +0300

    edit

--- js/main.js
+++ js/main.js
@@ -14,7 +14,20 @@ let
 	modalWindowHotKey	= document.querySelector('.modal-window-hot-key'),
 	closeWindow 		= document.querySelector('.close-window'),
 	rigthBar			= document.querySelector('.right-bar'),
-	rightBarBtn			= document.querySelector('.close-open-right-bar-btn');
+	rightBarBtn			= document.querySelector('.close-open-right-bar-btn'),
+	openVideoBtn 		= document.querySelector('.open-video-btn'),
+	youtubeVideo 		= document.querySelector('iframe'),
+	downListBtn 		= document.querySelector('.down-list-btn'),
+	btnCreateFile 		= document.querySelector('.btn-create-file'),
+	modalNewFolder 		= document.querySelector('.modal-window-new-folder'),
+	createFolder		= document.querySelector('.create-folder'),
+	inputName 			= document.querySelector('.input-name'),
+	btnFormNewFolder 	= document.querySelector('.btn-form-new-folder'),
+	file 				= document.querySelector('.file'),
+
+	folder,
+	span,
+	spanIcon;
 
 miniAccount.addEventListener('click', function() {
 	if (getComputedStyle(fullAccountInfo).display == 'none') {
@@ -106,10 +119,10 @@ buttonMenu[0].addEventListener('click', function() {
 	} else if (getComputedStyle(offlineMode).display != 'none') {
 		for (let i = 0; i < 1; i++) {
 			offlineMode.style.display = "none";
+			helpList.style.display = "none";
 			fullAccountInfo.style.display = "none";
 			apps.style.display = "none";
 			settingList.style.display = "none";
-			helpList.style.display = "none";
 		};
 	}
 });
@@ -166,4 +179,82 @@ rightBarBtn.addEventListener('click', function() {
 			rightBarBtn.style.transform = "rotate(180deg)";
 		}
 	}
+});
+
+openVideoBtn.addEventListener('click', function() {
+	if (getComputedStyle(youtubeVideo).display == "none") {
+		for(let i =  0; i < 1; i++) {
+			youtubeVideo.style.display = "block";
+			overlay.style.display = "block";
+		}
+	} 
+});
+
+openVideoBtn.addEventListener('keydown', function() {
+	for  (let i = 0; i < 1; i++) {
+		if (getComputedStyle(youtubeVideo).display != "none") {
+			for(let i =  0; i < 1; i++) {
+				youtubeVideo.style.display = "none";
+				overlay.style.display = "none";
+			}
+		} 
+	}
+});
+
+btnCreateFile.addEventListener('click', function() {
+	if (getComputedStyle(downListBtn).display == 'none') {
+		for (let i = 0; i < 1; i++) {
+			downListBtn.style.display = "block";
+			fullAccountInfo.style.display = "none";
+			apps.style.display = "none";
+			settingList.style.display = "none";
+			helpList.style.display = "none";
+		};
+	} else if (getComputedStyle(downListBtn).display != 'none') {
+		for (let i = 0; i < 1; i++) {
+			downListBtn.style.display = "none";
+			fullAccountInfo.style.display = "none";
+			apps.style.display = "none";
+			settingList.style.display = "none";
+			helpList.style.display = "none";
+		};
+	}
+});
+
+btnCreateFile.addEventListener('keydown', function() {
+	if (getComputedStyle(downListBtn).display != 'none') {
+		for (let i = 0; i < 1; i++) {
+			downListBtn.style.display = "none";
+			fullAccountInfo.style.display = "none";
+			apps.style.display = "none";
+			settingList.style.display = "none";
+			helpList.style.display = "none";
+		};
+	}
+});
+
+createFolder.addEventListener('click', function() {
+	for (let i = 0; i < 1; i++) {
+		modalNewFolder.style.display = "block";
+		downListBtn.style.display = "none";
+		overlay.style.display = "block";
+
+		folder 		= document.createElement('div'),
+		span    	= document.createElement('span'),
+		spanIcon 	= document.createElement('span');	
+		
+		spanIcon.classList.add('fas');
+		spanIcon.classList.add('fa-folder');
+		folder.classList.add('folder');
+
+		file.appendChild(folder);
+		folder.appendChild(spanIcon);	 
+		folder.appendChild(span);
+	}
+});
+
+btnFormNewFolder.addEventListener('click', function() {
+	span.textContent = inputName.value;
+	overlay.style.display = "none";
+	modalNewFolder.style.display = "none";
 });
